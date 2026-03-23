f1ee87ee5abe625cbd1d99bb47207064383365e5
FernandoGrigoletto <70235369+FernandoGrigoletto@users.noreply.github.com>
2026-03-23 08:07:48 -0300

    Update disciplinaController.js

--- src/controllers/disciplinaController.js
+++ src/controllers/disciplinaController.js
@@ -1,1 +1,13 @@
 
+const pool = require('../config/db');
+
+async function listar(req, res) {
+    try {
+        const [rows] = await pool.query('SELECT * FROM disciplinas');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar disciplinas no banco de dados' });
+    }
+    
+}
+module.exports = { listar };
