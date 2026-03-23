5c4d6eed3fe0350718debbdfee0f45e6d4c7b5c8
FernandoGrigoletto <70235369+FernandoGrigoletto@users.noreply.github.com>
2026-03-23 07:50:55 -0300

    Fix typo in disciplinaRoutes import and usage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const disciplinaRoutes = require('.routes/disciplinaRoutes');
+const displinaRoutes = require('./routes/disciplinaRoutes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/disciplinas', disciplinaRoutes);
+app.use('/disciplinas', displinaRoutes);
 
 app.listen(PORT, () => {
-    console.log('Servidor rodando em http://localhost:${PORT}');
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
