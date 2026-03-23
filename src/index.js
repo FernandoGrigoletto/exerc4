011cd7735cec5519a5b4e5de6ca5e610d0611c18
FernandoGrigoletto <70235369+FernandoGrigoletto@users.noreply.github.com>
2026-03-23 01:08:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const disciplinaRoutes = require('./disciplinaRoutes');
+import express from 'express';
+import disciplinaRoutes from './disciplinaRoutes.js'; // Não esqueça o .js no final!
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Rota principal para testar
+app.get('/', (req, res) => {
+    res.send('API de Disciplinas Rodando!');
+});
+
 app.use('/disciplinas', disciplinaRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
