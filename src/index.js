c0726ace4d49e7fd61956d72e706f49d39328ca0
FernandoGrigoletto <70235369+FernandoGrigoletto@users.noreply.github.com>
2026-03-23 07:47:26 -0300

    Refactor index.js for CommonJS and cleanup
    Updated import statements to CommonJS syntax and removed test route.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import express from 'express';
-import disciplinaRoutes from './routes/disciplinaRoutes.js'; // ⚠️ IMPORTANTE: Adicione o .js no final do caminho
+const express = require('express');
+const disciplinaRoutes = require('.routes/disciplinaRoutes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-// Rota principal para teste rápido
-app.get('/', (req, res) => {
-    res.send('API de Disciplinas rodando com ES Modules!');
-});
-
 app.use('/disciplinas', disciplinaRoutes);
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando com sucesso na porta ${PORT}`);
-    console.log(`Acesse: http://localhost:${PORT}/disciplinas`);
+    console.log('Servidor rodando em http://localhost:${PORT}');
 });
