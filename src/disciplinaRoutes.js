bcbcbeb6a24ac81eee6f7a09f09299a0e1d1a4cc
FernandoGrigoletto <70235369+FernandoGrigoletto@users.noreply.github.com>
2026-03-23 00:19:53 -0300

    Delete src/disciplinaRoutes.js

--- src/disciplinaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const disciplinaController = require('../controllers/disciplinaController');
-
-router.get('/', disciplinaController.listen);
-
-module.exports = router;
